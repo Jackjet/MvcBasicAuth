22b4c3b09c5a60e332713d08dac0ba74876e970c
Ilya Verbitskiy <iverbitskiy@gmail.com>
2017-03-20 11:14:56 +0100

    Sample authentication service.

---
+++ src/SampeWebSite/Infrastructure/AuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using MvcBasicAuth;
+
+namespace SampeWebSite.Infrastructure
+{
+    public class AuthenticationService : IHttpBasicAuthentication
+    {
+        private const string Username = "user";
+
+        private const string Password = "password";
+
+        public string Realm => "MvcBasicAuth Sample Application";
+
+        public bool Authenticate(string username, string password)
+        {
+            return username == Username && password == Password;
+        }
+    }
+}
